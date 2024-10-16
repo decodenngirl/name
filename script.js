62ef78ea2409c321c17cd3daec8d7b524f88ea5d
goyaguapo <decodenngirl@gmail.com>
2024-10-16 10:32:10 -0600

    carrusel

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+
+function moveSlides(direction) {
+    const slides = document.querySelector('.carousel');
+    const totalSlides = slides.children.length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
